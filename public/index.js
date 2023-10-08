356773cf373c34668ce051b15ec04975f4cd31b3
Natalievo1 <natalieojeda@icloud.com>
2023-10-07 20:25:37 -0700

    adding API fetch request

--- public/index.js
+++ public/index.js
@@ -6,4 +6,11 @@ async function main() {
 
 }
 
-main()
+main() {
+
+        const response = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX,TRP:TSX&interval=1min&apikey=9b07f38668e84a77901476ef8972a46a");
+        const stocks = await response.json();
+        console.log(stocks);
+      
+      
+}
